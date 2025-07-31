9655d6fa3d9c51c1e337abae3459b3ca0ffe5627
Higor <hfashigor@gmail.com>
2025-07-31 18:50:11 -0300

    Starting with vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    
+    let v1: Vec<i32> = Vec::new();
+    let v2 = vec![1, 3, 3];
+
+    let mut v3 = Vec::new();
+
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+
+    println!("v1: {:?}", v1);
+    println!("v2: {:?}", v2);
+    println!("v3: {:?}", v3);
 }
