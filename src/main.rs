7670f6b78cfbb9c2cba8f99c2728e182771327d1
Higor <hfashigor@gmail.com>
2025-08-01 19:32:02 -0300

    Collections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
+use std::collections::HashMap;
+
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3  = s1 + &s2;
-    println!("{}", s3);
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("Team: {team_name}, Score: {score}");
+
+    println!("Iterating over scores:");
+    for (team, score) in &scores {
+        println!("{team} --> {score}");
+    }
+
+    println!("\n");
+
+    {
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+
+        scores.entry(String::from("Yellow")).or_insert(50);
+        scores.entry(String::from("Blue")).or_insert(50);
+
+        println!("{scores:?}");
+    }
+    println!("\n");
+
+    {
+        let text = "hello world wonderful world";
+
+        let mut map = HashMap::new();
+
+        for word in text.split_whitespace() {
+            let count = map.entry(word).or_insert(0);
+            *count += 1;
+        }
+
+        println!("{map:?}")
+    }
 }
