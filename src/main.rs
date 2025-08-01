70ce5676c117575603506f17c4114305bfd910ef
Higor <hfashigor@gmail.com>
2025-08-01 14:10:30 -0300

    Itarating over vectors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 fn main() {
-    let v1: Vec<i32> = Vec::new();
-    let v2 = vec![1, 3, 3];
+    
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+    println!("{:?}", v);
 
-    let mut v3 = Vec::new();
-
-    v3.push(5);
-    v3.push(6);
-    v3.push(7);
-    v3.push(8);
-
-    println!("v1: {:?}", v1);
-    println!("v2: {:?}", v2);
-    println!("v3: {:?}", v3);
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+    println!("{:?}", v);
 }
