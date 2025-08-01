490d63aa68213526542e05da0f0486bc7cf7254e
Higor <hfashigor@gmail.com>
2025-08-01 20:49:01 -0300

    Matching on Different Errors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use std::fs::File;
+use std::{fs::File, io::{ErrorKind, Write}};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(mut fc) => {
+                    let content = "Hello, world!".as_bytes();
+                    match fc.write_all(content) {
+                        Ok(_) => fc,
+                        Err(e) => panic!("Problem while writing to hello.txt: {e:?}")
+                    }
+                },
+                Err(e) =>  panic!("Problem creating the file: {e:?}")
+            },
+            _ => {
+                panic!("Problem oppening the file {error:?}")
+            }
+        }
     };
 
     println!("{:?}", greeting_file);
