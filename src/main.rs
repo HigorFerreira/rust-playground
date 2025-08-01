0b471a304933dd10adda12360578e44c49dc4351
Higor <hfashigor@gmail.com>
2025-08-01 15:08:43 -0300

    Dropping vector

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-fn main() {
-    
-    let v = vec![100, 32, 57];
-    for i in &v {
-        println!("{i}");
-    }
-    println!("{:?}", v);
+use std::iter::Enumerate;
 
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
+fn main() {
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
-    println!("{:?}", v);
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    {
+        println!("Inside scope of v");        
+        let v = vec![100, 32, 57];
+
+        for i in &v {
+            println!("V elment: {i}");
+        }
+    }
+    println!("Outside scope of v");
+    // println!("{v:?}", v); // This will not compile, as v is out of scope here
 }
