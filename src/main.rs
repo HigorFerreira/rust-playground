048450a91f06f3022bc4089a67a0b6bcfaf6e5f1
Higor <hfashigor@gmail.com>
2025-08-01 16:54:08 -0300

    String moved and assigned reference

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
-use std::iter::Enumerate;
-
 fn main() {
-
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-    {
-        println!("Inside scope of v");        
-        let v = vec![100, 32, 57];
-
-        for i in &v {
-            println!("V elment: {i}");
-        }
-    }
-    println!("Outside scope of v");
-    // println!("{v:?}", v); // This will not compile, as v is out of scope here
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3  = s1 + &s2;
+    println!("{}", s3);
 }
