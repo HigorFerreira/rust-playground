d93fd68c55763d93b0d8c5b5da220382155d2754
Higor <hfashigor@gmail.com>
2025-08-04 12:48:28 -0300

    Propagating Errors

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use std::{fs::File, io::{ErrorKind, Write}};
+use std::{fs::File, io::{self, ErrorKind, Read, Write}};
 
 fn main() {
-    
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            match File::create("hello.txt") {
-                Err(error) => panic!("Problem creating the file: {error:?}"),
-                Ok(mut fc) => {
-                    if let Err(error) = fc.write_all("Hello, world!".as_bytes()) {
-                        panic!("Problem while writing to hello.txt: {error:?}")
-                    } else {
-                        fc
-                    }
-                }
-            }
-        } else {
-            panic!("Problem oppening the file {error:?}")
-        }
-    });
+    let username: Option<String> = match read_username_from_file() {
+        Ok(r) => Some(r),
+        Err(_) => None
+    };
 
-    println!("{:?}", greeting_file);
+    println!("{}", match username {
+        Some(str) => str,
+        None => String::from("")
+    })
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e)
+    }
 }
