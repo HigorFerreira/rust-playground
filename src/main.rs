5f2099fbbb22d1c62722bf7a5cfd88c7bbe1ab65
Higor <hfashigor@gmail.com>
2025-08-04 11:26:16 -0300

    Alternatives to Using match with Result<T, E>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
 use std::{fs::File, io::{ErrorKind, Write}};
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
+    
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            match File::create("hello.txt") {
+                Err(error) => panic!("Problem creating the file: {error:?}"),
                 Ok(mut fc) => {
-                    let content = "Hello, world!".as_bytes();
-                    match fc.write_all(content) {
-                        Ok(_) => fc,
-                        Err(e) => panic!("Problem while writing to hello.txt: {e:?}")
+                    if let Err(error) = fc.write_all("Hello, world!".as_bytes()) {
+                        panic!("Problem while writing to hello.txt: {error:?}")
+                    } else {
+                        fc
                     }
-                },
-                Err(e) =>  panic!("Problem creating the file: {e:?}")
-            },
-            _ => {
-                panic!("Problem oppening the file {error:?}")
+                }
             }
+        } else {
+            panic!("Problem oppening the file {error:?}")
         }
-    };
+    });
 
     println!("{:?}", greeting_file);
 }
