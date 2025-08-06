ff51c7e7479f1e7ec1e6bd0b9244f363ef82e676
Higor <hfashigor@gmail.com>
2025-08-06 19:06:15 -0300

    Made lasgest generic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
     for number in list {
         if number > largest {
@@ -13,6 +13,6 @@ fn main() {
     let number_list = vec![34, 50, 25, 100, 102, 101, 120, 65];
     println!("The largest number is {}", largest(&number_list));
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    println!("The largest number is {}", largest(&number_list));
+    let number_list = vec!['H', 'i', 'g', 'o', 'r'];
+    println!("The largest char is {}", largest(&number_list));
 }
