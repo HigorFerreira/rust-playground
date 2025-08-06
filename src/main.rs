62942b8e75df2a11dc882ec4c06f08b4a20580fd
Higor <hfashigor@gmail.com>
2025-08-06 19:06:03 -0300

    Lasgest fn

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 102, 101, 120, 65];
+    println!("The largest number is {}", largest(&number_list));
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    println!("The largest number is {}", largest(&number_list));
+}
