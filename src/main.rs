04fe59ef1d60eb56592a1397ec14cb19b87828d0
Higor <hfashigor@gmail.com>
2025-08-06 15:26:29 -0300

    Adjusts

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use std::fs;
-use std::io;
+use std::{net::IpAddr, str::FromStr};
 
 fn main() {
-    let username: Option<String> = match read_username_from_file() {
-        Ok(r) => Some(r),
-        Err(_) => None
+    let home: Option<IpAddr> = match "127.0.0.1".parse() {
+        Ok(addr) => Some(addr),
+        _ => None
     };
 
-    println!("{}", match username {
-        Some(str) => str,
-        None => String::from("")
-    })
-}
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
-}
-
-fn last_char_of_first_line(text: &str) -> Option<char> {
-    text.lines().next()?.chars().last()
+    println!("{home:?}");
 }
