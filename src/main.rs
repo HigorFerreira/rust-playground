2b416e887926c93360df1a3af5598cb6cd22826b
Higor <hfashigor@gmail.com>
2025-08-06 15:38:04 -0300

    Recovering guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use std::{net::IpAddr, str::FromStr};
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
 
 fn main() {
-    let home: Option<IpAddr> = match "127.0.0.1".parse() {
-        Ok(addr) => Some(addr),
-        _ => None
-    };
+    println!("Guess the number!");
 
-    println!("{home:?}");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
